744a3323a14c1d2b703ace09f6e320b606526e91
VsCode_Marcelo <you@example.com>
2025-12-19 16:25:41 -0300

    fix: mastering filter

--- bloco2/aula6_filtragem_dados.py
+++ bloco2/aula6_filtragem_dados.py
@@ -7,10 +7,17 @@ df["faturamento"] = df["quantidade"] * df["valor_unitario"]
 print("=== Todas as vendas ===")
 print(df)
 
+print("\n=== Apenas Eletrônicos ===")
+print(df[df["categoria"] == "Eletrônicos"])
+
+print("\n=== Quantidade >= 3 ===")
+print(df[df["quantidade"] >= 3])
+
+print("\n=== Periféricos com quantidade >= 4 ===")
+
 filtro_categoria = df["categoria"] == "Periféricos"
 filtro_quantidade = df["quantidade"] >= 4
 
 df_filtrado = df[filtro_categoria & filtro_quantidade]
 
-print("\n=== Periféricos com quantidade >= 4 ===")
 print(df_filtrado)
